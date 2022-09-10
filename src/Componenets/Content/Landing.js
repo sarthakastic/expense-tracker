2cd2690fdd50ccefcbb589fee824432f1e0cb0fd
sarthak2605 <iamsg26@gmail.com>
2022-09-11 02:15:05 +0530

    particle js added

--- src/Componenets/Content/Landing.js
+++ src/Componenets/Content/Landing.js
@@ -1,43 +1,62 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
+import Particles from "react-tsparticles";
 import LandingImage from "../../images/expense.svg";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
 
 const Landing = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    console.log(container);
+  }, []);
   return (
-    <div className="h-[calc(100vh-70px)] bg-[#031410] flex items-center ">
-      <div className="md:w-[50vw] align-middle ">
-        <div className="text-[32px] md:text-[62px] p-4 text-white font-bold">
-          Your Destination to
-        </div>
-
-        <div className="p-4 flex justify-center items-center input-cursor">
-          <div className="text-blue-200 font-extrabold text-[36px] md:text-[72px]">
-            <Typewriter
-              options={{
-                loop: true,
-                strings: [
-                  "Track your Expense",
-                  "Set monthly budget",
-                  "Manage Your Savings",
-                ],
-                autoStart: true,
-              }}
-              onInit={(typewriter) => {
-                typewriter
-
-                  .pauseFor(500)
-                  .deleteAll()
-
-                  .start();
-              }}
-            />
+    <div className="h-[calc(100vh-70px)] bg-[#031410] flex items-center">
+      <Particles
+        id="tsparticles"
+        url="/snow.json"
+        init={particlesInit}
+        loaded={particlesLoaded}
+      />
+        <div className="md:w-[50vw] align-middle ">
+          <div className="text-[32px] md:text-[62px] p-4 text-white font-bold">
+            Your Destination to
           </div>
-          
+
+          <div className="p-4 flex justify-center items-center input-cursor">
+            <div className="text-blue-200 font-extrabold text-[36px] md:text-[72px]">
+              <Typewriter
+                options={{
+                  loop: true,
+                  strings: [
+                    "Track your Expense",
+                    "Set monthly budget",
+                    "Manage Your Savings",
+                  ],
+                  autoStart: true,
+                }}
+                onInit={(typewriter) => {
+                  typewriter
+
+                    .pauseFor(500)
+                    .deleteAll()
+
+                    .start();
+                }}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="w-[50vw] h-full  hidden md:flex items-center justify-center">
-        <img src={LandingImage} alt="" className="h-96 w-96 p-10" />
-      </div>
+        <div className="w-[50vw] h-full  hidden md:flex items-center justify-center">
+          <img src={LandingImage} alt="" className="h-96 w-96 p-10" />
+        </div>
     </div>
   );
 };
--- src/Componenets/Navbar/Navbar.js
+++ src/Componenets/Navbar/Navbar.js
@@ -1,23 +1,28 @@
-import React from 'react'
-import logo from '../../images/logo.png'
-import Join from '../Button/Join'
-import ChangeTheme from '../theme/ChangeTheme'
+import React from "react";
+import logo from "../../images/logo.png";
+import Join from "../Button/Join";
+import ChangeTheme from "../theme/ChangeTheme";
 
 const Navbar = () => {
   return (
-    <div className='h-[70px] bg-[#031416] w-[100%] flex  items-center'>
-        <div className='flex items-center mx-2  w-full gap-2' >
-            <img src={logo} alt="logo" className='h-10 ' />
-            <p className='hidden md:flex w-full text-blue-200 text-xl font-extrabold' >Expense Tracker</p>
+    <div className="h-[70px] bg-[#031416] w-[100%] flex  items-center ">
+      <div className="flex items-center mx-2  w-full gap-2">
+        <img src={logo} alt="logo" className="h-10 " />
+        <p className="hidden md:flex w-full text-blue-200 text-xl font-extrabold">
+          Expense Tracker
+        </p>
+      </div>
 
+      <div className="flex gap-2">
+        <div className="mx-4 ">
+          <Join />
         </div>
-
-        <div className='flex gap-2'>
-        <div className='mx-4 '><Join/></div>
-        <div><ChangeTheme/></div>
+        <div>
+          <ChangeTheme />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
