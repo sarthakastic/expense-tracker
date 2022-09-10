ac684a0a9e36fc8e1ad0c61e09c188a9ef8373ed
sarthak2605 <iamsg26@gmail.com>
2022-09-10 20:39:07 +0530

    added typewriter effect

--- src/Componenets/Content/Landing.js
+++ src/Componenets/Content/Landing.js
@@ -1,41 +1,40 @@
-import React from 'react'
-import "./landing.css"
-import LandingImage from "../../images/expense.svg"
-
-
-async function typeSentence(sentence, eleRef, delay = 100) {
-    const letters = sentence.split("");
-    let i = 0;
-    while(i < letters.length) {
-      await waitForMs(delay);
-      $(eleRef).append(letters[i]);
-      i++
-    }
-    return;
-  }
-  
-  
-  function waitForMs(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
+import React from "react";
+import Typewriter from "typewriter-effect";
+import LandingImage from "../../images/expense.svg";
 
 const Landing = () => {
   return (
-    <div className='h-[calc(100vh-70px)] bg-[#031410] flex '>
-        <div className='md:w-[50vw] flex justify-center items-center'>
-            <span className='text-[62px] p-4 text-blue-200 font-extrabold'>Expense Tracker.</span><br />
-            <div className='flex justify-center items-center input-cursor'>
-    <span id="sentence" className=''>{typeSentence("hello", "#sentence")}</span>
-    <span className='inline-block w-0.5 h-11 bg-white ml-2'></span>
-  </div>
-        </div>
-        <div className='w-[50vw] h-full  hidden md:flex items-center justify-center'>
-            <img src={LandingImage} alt="" srcset="" className='h-96 w-96 p-10' />
-        </div>
+    <div className="h-[calc(100vh-70px)] bg-[#031410] flex ">
+      <div className="md:w-[50vw]  ">
+        <span className="text-[62px] p-4 text-white font-bold">
+          Your Destination to
+        </span>
 
+        <div className="flex justify-center items-center input-cursor">
+          <div className="text-blue-200 font-extrabold text-[72px]">
+            <Typewriter
+            options={{loop: true,strings: ["Track your Expense", "Set monthly budget", "Manage Your Savings"],
+            autoStart: true}}
+              onInit={(typewriter) => {
+                typewriter
+                  
 
+                  .pauseFor(500)
+                  .deleteAll()
+
+                  .start();
+                
+              }}
+            />
+          </div>
+          <span className="inline-block w-0.5 h-11 bg-white ml-2"></span>
+        </div>
+      </div>
+      <div className="w-[50vw] h-full  hidden md:flex items-center justify-center">
+        <img src={LandingImage} alt="" className="h-96 w-96 p-10" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
--- src/Componenets/theme/ChangeTheme.js
+++ src/Componenets/theme/ChangeTheme.js
@@ -3,9 +3,9 @@ import "./changetheme.css"
 
 const ChangeTheme = () => {
   return (
-    <label class="switch">
+    <label className="switch">
   <input type="checkbox"/>
-  <span class="slider round"></span>
+  <span className="slider round"></span>
 </label>
   )
 }
